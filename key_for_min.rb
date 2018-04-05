751e4c0ea084429f3309834d550484b6a50ce57a
Karen Flores <karen.ag.flores@gmail.com>
2018-04-05 06:54:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
+  i = 0
   min_start = name_hash.first[1]
   if name_hash.length == {}
     return nil
