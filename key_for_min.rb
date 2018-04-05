d9e2e08b82322bd47a37633109e6692e2924f0d1
Karen Flores <karen.ag.flores@gmail.com>
2018-04-05 07:22:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,13 +6,16 @@ def key_for_min_value(name_hash)
     return nil
   else
     num_start = name_hash.first[1]
+    min_key = name_hash.first[0]
     name_hash.each do |name, num|
       if num < num_start
         num_start = num
+        min_key = name
       else
         num_start
+        min_key
       end
     end
-    return name
+    return min_key
   end
 end
